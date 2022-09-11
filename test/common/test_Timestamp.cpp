e3d6f1563fe029e391d844fb0385d362f680736b
Florian Weßel <florianwessel@gmx.net>
2022-09-11 20:25:00 +0200

    Timestamp: Use msecs for subtraction

--- test/common/test_Timestamp.cpp
+++ test/common/test_Timestamp.cpp
@@ -20,16 +20,8 @@ TEST_CASE("A Timestamp shall be able to convert the time into a string.")
 
 TEST_CASE("It shall be possible to compare two timestamps for equal.")
 {
-    Timestamp ts1;
-    ts1.setHour(1);
-    ts1.setMinute(2);
-    ts1.setSecond(25);
-    ts1.setFractionalOfSecond(134);
-    Timestamp ts2;
-    ts2.setHour(1);
-    ts2.setMinute(2);
-    ts2.setSecond(25);
-    ts2.setFractionalOfSecond(134);
+    Timestamp ts1{"01:02:25.134"};
+    Timestamp ts2{"01:02:25.134"};
 
     REQUIRE(ts1 == ts2);
 }
@@ -156,22 +148,9 @@ TEST_CASE("The plus operator of timestamp shall handle millisecond overflow when
 
 TEST_CASE("The Timestamp shall support minus operations with a other Timestamp.")
 {
-    Timestamp ts1;
-    ts1.setHour(1);
-    ts1.setMinute(2);
-    ts1.setSecond(0);
-    ts1.setFractionalOfSecond(134);
-    Timestamp ts2;
-    ts2.setHour(3);
-    ts2.setMinute(5);
-    ts2.setSecond(25);
-    ts2.setFractionalOfSecond(144);
-
-    Timestamp expectedResult;
-    expectedResult.setHour(2);
-    expectedResult.setMinute(3);
-    expectedResult.setSecond(25);
-    expectedResult.setFractionalOfSecond(10);
+    Timestamp ts1{"01:02:00.134"};
+    Timestamp ts2{"03:05:25.144"};
+    Timestamp expectedResult{"02:03:25.010"};
 
     auto result = ts2 - ts1;
 
@@ -180,22 +159,9 @@ TEST_CASE("The Timestamp shall support minus operations with a other Timestamp."
 
 TEST_CASE("The Timestamp shall handle hour underflow in the minus operation.")
 {
-    Timestamp ts1;
-    ts1.setHour(5);
-    ts1.setMinute(0);
-    ts1.setSecond(0);
-    ts1.setFractionalOfSecond(0);
-    Timestamp ts2;
-    ts2.setHour(3);
-    ts2.setMinute(0);
-    ts2.setSecond(0);
-    ts2.setFractionalOfSecond(0);
-
-    Timestamp expectedResult;
-    expectedResult.setHour(22);
-    expectedResult.setMinute(0);
-    expectedResult.setSecond(0);
-    expectedResult.setFractionalOfSecond(0);
+    Timestamp ts1{"05:00:00.000"};
+    Timestamp ts2{"03:00:00.000"};
+    Timestamp expectedResult{"22:00:00.000"};
 
     auto result = ts2 - ts1;
 
@@ -204,22 +170,9 @@ TEST_CASE("The Timestamp shall handle hour underflow in the minus operation.")
 
 TEST_CASE("The Timestamp shall handle minute underflow in the minus operation.")
 {
-    Timestamp ts1;
-    ts1.setHour(1);
-    ts1.setMinute(48);
-    ts1.setSecond(0);
-    ts1.setFractionalOfSecond(0);
-    Timestamp ts2;
-    ts2.setHour(5);
-    ts2.setMinute(2);
-    ts2.setSecond(0);
-    ts2.setFractionalOfSecond(0);
-
-    Timestamp expectedResult;
-    expectedResult.setHour(4);
-    expectedResult.setMinute(14);
-    expectedResult.setSecond(0);
-    expectedResult.setFractionalOfSecond(0);
+    Timestamp ts1{"01:48:00.000"};
+    Timestamp ts2{"05:02:00.000"};
+    Timestamp expectedResult{"03:14:00.000"};
 
     auto result = ts2 - ts1;
 
@@ -228,22 +181,9 @@ TEST_CASE("The Timestamp shall handle minute underflow in the minus operation.")
 
 TEST_CASE("The Timestamp shall handle second underflow in the minus operation.")
 {
-    Timestamp ts1;
-    ts1.setHour(1);
-    ts1.setMinute(48);
-    ts1.setSecond(55);
-    ts1.setFractionalOfSecond(0);
-    Timestamp ts2;
-    ts2.setHour(5);
-    ts2.setMinute(2);
-    ts2.setSecond(5);
-    ts2.setFractionalOfSecond(0);
-
-    Timestamp expectedResult;
-    expectedResult.setHour(4);
-    expectedResult.setMinute(14);
-    expectedResult.setSecond(10);
-    expectedResult.setFractionalOfSecond(0);
+    Timestamp ts1{"01:48:55.000"};
+    Timestamp ts2{"05:02:55.000"};
+    Timestamp expectedResult{"03:14:00.000"};
 
     auto result = ts2 - ts1;
 
@@ -252,22 +192,9 @@ TEST_CASE("The Timestamp shall handle second underflow in the minus operation.")
 
 TEST_CASE("The Timestamp shall handle fractional of second underflow in the minus operation.")
 {
-    Timestamp ts1;
-    ts1.setHour(1);
-    ts1.setMinute(48);
-    ts1.setSecond(55);
-    ts1.setFractionalOfSecond(500);
-    Timestamp ts2;
-    ts2.setHour(5);
-    ts2.setMinute(2);
-    ts2.setSecond(5);
-    ts2.setFractionalOfSecond(128);
-
-    Timestamp expectedResult;
-    expectedResult.setHour(4);
-    expectedResult.setMinute(14);
-    expectedResult.setSecond(10);
-    expectedResult.setFractionalOfSecond(628);
+    Timestamp ts1{"01:48:55.000"};
+    Timestamp ts2{"05:02:05.128"};
+    Timestamp expectedResult{"03:13:10.128"};
 
     auto result = ts2 - ts1;
 
@@ -285,4 +212,12 @@ TEST_CASE("The Timestamp shall be creatable from string in the format of hh:mm:s
     auto ts = Timestamp{"01:48:55.500"};
 
     REQUIRE(ts == expectedTs);
+}
+
+TEST_CASE("The Timestamp shall correctly calucalute the delta between two timestamps")
+{
+    Timestamp ts1{"15:05:13.237"};
+    Timestamp ts2{"15:06:10.237"};
+
+    REQUIRE((ts2 - ts1) == Timestamp{"00:00:57.000"});
 }
