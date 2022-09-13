840e4f8473afecc3c1ef5144f5d1f446cff39846
Florian Weßel <florianwessel@gmx.net>
2022-09-13 21:27:31 +0200

    fSimpleLaptimer: Add positions for test in global class

--- test/dummy/Positions.hpp
+++ test/dummy/Positions.hpp
@@ -13,6 +13,21 @@ public:
     static Common::PositionData OscherslebenPositionStartFinishLine;
     static Common::PositionData OscherslebenPositionSector1Line;
     static Common::PositionData OscherslebenPositionSector2Line;
+
+    static Common::PositionData OscherslebenStartFinishLine1;
+    static Common::PositionData OscherslebenStartFinishLine2;
+    static Common::PositionData OscherslebenStartFinishLine3;
+    static Common::PositionData OscherslebenStartFinishLine4;
+
+    static Common::PositionData OscherslebenSector1Point1;
+    static Common::PositionData OscherslebenSector1Point2;
+    static Common::PositionData OscherslebenSector1Point3;
+    static Common::PositionData OscherslebenSector1Point4;
+
+    static Common::PositionData OscherslebenSector2Point1;
+    static Common::PositionData OscherslebenSector2Point2;
+    static Common::PositionData OscherslebenSector2Point3;
+    static Common::PositionData OscherslebenSector2Point4;
 };
 
 } // namespace LaptimerCore::Test::Dummy
