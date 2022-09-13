049ea4565da8ab97df1e9f1e3c3383ee2b5d4945
Florian Weßel <florianwessel@gmx.net>
2022-09-13 21:27:31 +0200

    fSimpleLaptimer: Add positions for test in global class

--- test/dummy/Positions.cpp
+++ test/dummy/Positions.cpp
@@ -10,4 +10,19 @@ PositionData Positions::OscherslebenPositionStartFinishLine{52.0270889, 11.28034
 PositionData Positions::OscherslebenPositionSector1Line{52.0298205, 11.2741851};
 PositionData Positions::OscherslebenPositionSector2Line{52.0299681, 11.2772076};
 
+PositionData Positions::OscherslebenStartFinishLine1{52.0270444, 11.2805431};
+PositionData Positions::OscherslebenStartFinishLine2{52.0270730, 11.2804234};
+PositionData Positions::OscherslebenStartFinishLine3{52.0271084, 11.2802563};
+PositionData Positions::OscherslebenStartFinishLine4{52.0271438, 11.2800835};
+
+PositionData Positions::OscherslebenSector1Point1{52.029819, 11.274203};
+PositionData Positions::OscherslebenSector1Point2{52.029821, 11.274193};
+PositionData Positions::OscherslebenSector1Point3{52.029821, 11.274169};
+PositionData Positions::OscherslebenSector1Point4{52.029822, 11.274149};
+
+PositionData Positions::OscherslebenSector2Point1{52.029970, 11.277183};
+PositionData Positions::OscherslebenSector2Point2{52.029968, 11.277193};
+PositionData Positions::OscherslebenSector2Point3{52.029967, 11.277212};
+PositionData Positions::OscherslebenSector2Point4{52.029966, 11.277218};
+
 } // namespace LaptimerCore::Test::Dummy
