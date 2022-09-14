fb3322ef43326a009a88e2b35d8186662a8f06cb
Florian Weßel <florianwessel@gmx.net>
2022-09-14 20:34:29 +0200

    SSD1322Shell: Pass PositionDateTimeProvider for the active worksession flow

---
+++ src/positioning/include/StaticPositionDateTimeProvider.hpp
@@ -0,0 +1,25 @@
+#ifndef STATICPOSITIONDATETIMEPROVIDER_HPP
+#define STATICPOSITIONDATETIMEPROVIDER_HPP
+
+#include "IPositionDateTimeProvider.hpp"
+#include <Timestamp.hpp>
+
+namespace LaptimerCore::Positioning
+{
+
+class StaticPositionDateTimeProvider final : public IPositionDateTimeProvider
+{
+public:
+    StaticPositionDateTimeProvider()
+    {
+        positionTimeData.set(Common::PositionDateTimeData{{52.0270889, 11.2803483}, Common::Timestamp{"15:05:10.234"}, {}});
+    }
+
+    ~StaticPositionDateTimeProvider()
+    {
+    }
+};
+
+} // namespace LaptimerCore::Positioning
+
+#endif // STATICPOSITIONDATETIMEPROVIDER_HPP
