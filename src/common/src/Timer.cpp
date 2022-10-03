13f46cea34333c7bc892876ab17a3ec6b47d5dd9
Florian Weßel <florianwessel@gmx.net>
2022-10-03 21:18:27 +0200

    Common: Implement a very very basic timer

---
+++ src/common/src/Timer.cpp
@@ -0,0 +1,88 @@
+#include "Timer.hpp"
+#include <unordered_set>
+
+namespace LaptimerCore::Common
+{
+
+struct TimerRegister
+{
+    std::unordered_set<Timer *> timers;
+
+    void handleAllTimerTicks()
+    {
+        for (const auto &timer : timers)
+        {
+            if (timer->isRunning())
+            {
+                timer->handleTicks();
+            }
+        }
+    }
+};
+
+namespace
+{
+auto s_timers = TimerRegister{};
+} // namespace
+
+void handleTimerTicks()
+{
+    s_timers.handleAllTimerTicks();
+}
+
+Timer::Timer()
+{
+    s_timers.timers.insert(this);
+}
+
+Timer::~Timer()
+{
+    if (mRunning)
+    {
+        stop();
+    }
+    s_timers.timers.erase(this);
+}
+
+void Timer::start()
+{
+    if (mRunning || (mInterval == std::chrono::milliseconds(0)))
+    {
+        return;
+    }
+
+    mStartTime = std::chrono::steady_clock::now();
+    mRunning = true;
+}
+
+void Timer::stop()
+{
+    mRunning = false;
+}
+
+void Timer::setInterval(std::chrono::milliseconds interval)
+{
+    mInterval = interval;
+    if (mRunning)
+    {
+        stop();
+        start();
+    }
+}
+
+bool Timer::isRunning()
+{
+    return mRunning;
+}
+
+void Timer::handleTicks()
+{
+    const auto elapsedTime =
+        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - mStartTime).count();
+    if (elapsedTime >= mInterval.count())
+    {
+        timeout.emit();
+    }
+}
+
+} // namespace LaptimerCore::Common
---
+++ test/common/test_Timer.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "Timer.hpp"
+#include <catch2/catch.hpp>
+#include <thread>
+
+using namespace LaptimerCore::Common;
+
+TEST_CASE("The timer shall emit the timeout event after the elapsed time set by the interval.")
+{
+    auto timeoutEventEmitted = false;
+    auto timer = Timer{};
+    timer.setInterval(std::chrono::milliseconds(3));
+    timer.timeout.connect([&]() { timeoutEventEmitted = true; });
+
+    timer.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(3));
+    handleTimerTicks();
+
+    REQUIRE(timeoutEventEmitted == true);
+}
