57f4572853c31ae058221fc806ba10486c328f9e
Florian Weßel <florianwessel@gmx.net>
2022-10-09 00:41:32 +0200

    QtShell: Start/Stop GPS source

--- src/shell/qt/src/MainWindowViewModel.cpp
+++ src/shell/qt/src/MainWindowViewModel.cpp
@@ -11,6 +11,7 @@ struct MainWindowViewModelPrivate
 {
     Positioning::ConstantVelocityPositionDateTimeProvider gpsSource;
     QGeoCoordinate currentPosition;
+    bool gpsSourceIsActive{false};
 };
 
 MainWindowViewModel::MainWindowViewModel()
@@ -18,7 +19,7 @@ MainWindowViewModel::MainWindowViewModel()
     , d{std::make_unique<MainWindowViewModelPrivate>()}
 {
     d->gpsSource.positionTimeData.valueChanged().connect(&MainWindowViewModel::handlePositionUpdate, this);
-    d->gpsSource.setVelocityInMeterPerSecond(2.77778);
+    d->gpsSource.setVelocityInMeterPerSecond(41.6667);
 }
 
 MainWindowViewModel::~MainWindowViewModel() = default;
@@ -26,6 +27,15 @@ MainWindowViewModel::~MainWindowViewModel() = default;
 void MainWindowViewModel::startGpsSource()
 {
     d->gpsSource.start();
+    d->gpsSourceIsActive = true;
+    Q_EMIT gpsSourceActiveChanged();
+}
+
+void MainWindowViewModel::stopGpsSource()
+{
+    d->gpsSource.stop();
+    d->gpsSourceIsActive = false;
+    Q_EMIT gpsSourceActiveChanged();
 }
 
 void MainWindowViewModel::loadGpsFile(const QUrl &fileName)
@@ -45,13 +55,15 @@ QGeoCoordinate MainWindowViewModel::getCurrentPosition() const noexcept
     return d->currentPosition;
 }
 
+bool MainWindowViewModel::isGpsSourceActive()
+{
+    return d->gpsSourceIsActive;
+}
+
 void MainWindowViewModel::handlePositionUpdate()
 {
     auto pos = d->gpsSource.positionTimeData.get();
     d->currentPosition = QGeoCoordinate{pos.getPosition().getLatitude(), pos.getPosition().getLongitude()};
-
-    //    qInfo() << d->currentPosition;
-
     Q_EMIT currentPositionChanged();
 }
 
