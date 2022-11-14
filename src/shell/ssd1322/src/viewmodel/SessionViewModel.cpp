ad00feac6fcdeec8cc2b5f29b979d0265ce86aa7
Florian Weßel <florianwessel@gmx.net>
2022-11-14 21:18:12 +0100

    SSD1322 Shell: Handle changes in the SessionDatabase and update UI.

--- src/shell/ssd1322/src/viewmodel/SessionViewModel.cpp
+++ src/shell/ssd1322/src/viewmodel/SessionViewModel.cpp
@@ -8,28 +8,30 @@ SessionViewModel::SessionViewModel(LaptimerCore::Session::ISessionDatabase &sess
     , mLapView{mLapModel}
     , mLapViewCloseCommand(*this)
 {
-    if (mSessionDb.getSessionCount() == 0)
-    {
-        return;
-    }
-
+    mSessionDb.sessionAdded.connect(&SessionViewModel::onSessionAdded, this);
+    mSessionDb.sessionUpdated.connect(&SessionViewModel::onSessionUpdated, this);
     mLapView.setCloseCommand(&mLapViewCloseCommand);
-    setSize(mSessionDb.getSessionCount());
-    displaySession(mSessionDb.getSessionCount() - 1);
+
+    // display initial session
+    if (mSessionDb.getSessionCount() != 0)
+    {
+        setSize(mSessionDb.getSessionCount());
+        displaySession(mSessionDb.getSessionCount() - 1);
+    }
 }
 
 SessionViewModel::~SessionViewModel() = default;
 
 void SessionViewModel::handleNavigationUp()
 {
-    incrementIndex();
-    displaySession(getIndex());
+    decrementIndex();
+    displaySession(getSize() - getIndex() - 1);
 }
 
 void SessionViewModel::handleNavigationDown()
 {
-    decrementIndex();
-    displaySession(getIndex());
+    incrementIndex();
+    displaySession(getSize() - getIndex() - 1);
 }
 
 void SessionViewModel::open()
@@ -59,4 +61,28 @@ void SessionViewModel::displaySession(std::size_t index)
     time.set(session->getSessionTime().asString());
     laps.set(std::to_string(session->getNumberOfLaps()));
     bestlap.set(session->getLaps().at(0).getLaptime().asString());
+}
+
+void SessionViewModel::onSessionAdded()
+{
+    const auto oldIndex = getIndex();
+    setSize(mSessionDb.getSessionCount());
+
+    // If the view is on the top off all session we have to switch to the new session
+    // and if the view is not on the top position we just stay at that position.
+    if (oldIndex == 0)
+    {
+        displaySession(mSessionDb.getSessionCount() - 1);
+    }
+}
+
+void SessionViewModel::onSessionUpdated(std::size_t index)
+{
+    // The ordering of the session in the oppsite to the index of the elements in the view
+    // index = 0 is equal session index = max. That the reason for subtraction. If
+    // the index is the same then session is displayed to the user and must be updated.
+    if (index == (getSize() - 1) - getIndex())
+    {
+        displaySession(index);
+    }
 }
