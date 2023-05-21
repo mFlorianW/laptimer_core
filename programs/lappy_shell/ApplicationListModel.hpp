70a7a55e2191d86f7a2321d2de07a60311bc5cf6
Florian Weßel <florianwessel@gmx.net>
2023-05-21 22:00:26 +0200

    Programs: lappy_shell introduce list model for applications

---
+++ programs/lappy_shell/ApplicationListModel.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "ApplicationModel.hpp"
+#include <QAbstractListModel>
+
+namespace LaptimerCore::LappyShell
+{
+
+class ApplicationListModel final : public QAbstractListModel
+{
+    Q_OBJECT
+    Q_DISABLE_COPY_MOVE(ApplicationListModel)
+public:
+    enum DisplayRole
+    {
+        AppName = Qt::DisplayRole + 1,
+        IconUrl,
+    };
+    Q_ENUM(DisplayRole)
+
+    ApplicationListModel(ApplicationModel &appModel) noexcept;
+    ~ApplicationListModel() noexcept override = default;
+
+    qint32 rowCount(QModelIndex const &parent) const noexcept override;
+    QVariant data(QModelIndex const &index, qint32 displayRole) const noexcept override;
+    QHash<qint32, QByteArray> roleNames() const noexcept override;
+
+private:
+    ApplicationModel &mAppModel;
+};
+} // namespace LaptimerCore::LappyShell
---
+++ tests/lappy_shell/test_ApplicationListModel.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_MAIN
+#include "ApplicationListModel.hpp"
+#include "FilePath.hpp"
+#include <QAbstractItemModelTester>
+#include <catch2/catch.hpp>
+
+using namespace LaptimerCore::LappyShell;
+
+SCENARIO("The ApplicationListModel shall return the correct values for the UI")
+{
+    auto appModel = ApplicationModel{{CONFIG_APP_PATH}};
+    appModel.loadApplications();
+    auto listModel = ApplicationListModel{appModel};
+    auto modelTester = QAbstractItemModelTester{&listModel};
+    WHEN("The values for the UI are requested")
+    {
+        const auto appName = listModel.data(listModel.index(0), ApplicationListModel::AppName);
+        const auto iconUrl = listModel.data(listModel.index(0), ApplicationListModel::IconUrl);
+        const auto count = listModel.rowCount(QModelIndex{});
+        THEN("The correct values shall be returned.")
+        {
+            REQUIRE(count == 2);
+            REQUIRE(appName == "Testapp1");
+            REQUIRE(iconUrl == "/usr/share/lappy/test/icon.png");
+        }
+    }
+}
