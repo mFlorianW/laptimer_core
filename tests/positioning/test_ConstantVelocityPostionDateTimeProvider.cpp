60da5d510bec2c79b0491ffee3c458e48ff2c089
Florian Weßel <florianwessel@gmx.net>
2024-05-20 17:54:25 +0200

    Timer: Change to posix timer and eventloop based implementation

--- tests/positioning/test_ConstantVelocityPostionDateTimeProvider.cpp
+++ tests/positioning/test_ConstantVelocityPostionDateTimeProvider.cpp
@@ -3,21 +3,21 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #define CATCH_CONFIG_MAIN
+#include "CompareHelper.hpp"
 #include "ConstantVelocityPositionDateTimeProvider.hpp"
-#include "SignalDispatcher.hpp"
 #include <PositionData.hpp>
 #include <catch2/catch.hpp>
-#include <thread>
 
 using namespace LaptimerCore::Positioning;
 using namespace LaptimerCore::Common;
 using namespace LaptimerCore::System;
+using namespace LaptimerCore::Testhelper;
+
+constexpr auto timeout = std::chrono::seconds{1};
 
 TEST_CASE("The ConstantVelocityPositionDateTimeProvider shall interpolate the position between two points if the given "
           "point data is insufficent.")
 {
-    auto dispatcher = SignalDispatcher{};
-    auto const timeToSleep = std::chrono::milliseconds(100);
     auto positions = std::vector<PositionData>{{52.026649, 11.282535}, {52.026751, 11.282047}, {52.026807, 11.281746}};
     auto lastPosition = PositionDateTimeData{};
 
@@ -28,23 +28,21 @@ TEST_CASE("The ConstantVelocityPositionDateTimeProvider shall interpolate the po
     source.setVelocityInMeterPerSecond(2.77778);
     source.start();
 
-    std::this_thread::sleep_for(timeToSleep);
-    dispatcher.exec();
     auto expectedPosition = PositionDateTimeData{{52.026649, 11.282535}, {}, {}};
-    REQUIRE(lastPosition.getPosition().getLatitude() == expectedPosition.getPosition().getLatitude());
+    REQUIRE_COMPARE_WITH_TIMEOUT(lastPosition.getPosition().getLatitude(),
+                                 expectedPosition.getPosition().getLatitude(),
+                                 timeout);
     REQUIRE(lastPosition.getPosition().getLongitude() == expectedPosition.getPosition().getLongitude());
 
-    std::this_thread::sleep_for(timeToSleep);
-    dispatcher.exec();
     expectedPosition = PositionDateTimeData{{52.02665, 11.28253}, {}, {}};
-    REQUIRE(lastPosition.getPosition().getLatitude() == expectedPosition.getPosition().getLatitude());
+    REQUIRE_COMPARE_WITH_TIMEOUT(lastPosition.getPosition().getLatitude(),
+                                 expectedPosition.getPosition().getLatitude(),
+                                 timeout);
     REQUIRE(lastPosition.getPosition().getLongitude() == Approx(expectedPosition.getPosition().getLongitude()));
 }
 
 TEST_CASE("The ConstantVelocityPositionDateTimeProvider shall provide every 100ms a new position.")
 {
-    auto dispatcher = SignalDispatcher{};
-    auto const timeToSleep = std::chrono::milliseconds(100);
     std::uint8_t updateCounter = 0;
     auto positions = std::vector<PositionData>{{52.026649, 11.282535}, {52.026751, 11.282047}, {52.026807, 11.281746}};
     auto source = ConstantVelocityPositionDateTimeProvider{positions};
@@ -52,18 +50,12 @@ TEST_CASE("The ConstantVelocityPositionDateTimeProvider shall provide every 100m
     source.positionTimeData.valueChanged().connect([&]() {
         ++updateCounter;
     });
+    // update postion when source is started
     source.start();
+    REQUIRE_COMPARE_WITH_TIMEOUT(updateCounter, 1, timeout);
+    REQUIRE_COMPARE_WITH_TIMEOUT(updateCounter, 2, timeout);
 
-    std::this_thread::sleep_for(timeToSleep);
-    dispatcher.exec();
-    REQUIRE(updateCounter == 1);
-
-    std::this_thread::sleep_for(timeToSleep);
-    dispatcher.exec();
-    REQUIRE(updateCounter == 2);
-
+    // no updates when source is stopped
     source.stop();
-    std::this_thread::sleep_for(timeToSleep);
-    dispatcher.exec();
-    REQUIRE(updateCounter == 2);
+    REQUIRE_COMPARE_WITH_TIMEOUT(updateCounter, 2, timeout);
 }
--- tests/system/test_Timer.cpp
+++ tests/system/test_Timer.cpp
@@ -3,26 +3,47 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #define CATCH_CONFIG_MAIN
-#include "SignalDispatcher.hpp"
+#include "CompareHelper.hpp"
+#include "EventLoop.hpp"
 #include "Timer.hpp"
 #include <catch2/catch.hpp>
-#include <thread>
 
 using namespace LaptimerCore::System;
+using namespace LaptimerCore::Testhelper;
+using namespace std::chrono;
 
 TEST_CASE("The timer shall emit the timeout event after the elapsed time set by the interval.")
 {
-    auto dispatcher = SignalDispatcher{};
+    auto eventloop = EventLoop{};
+
     auto timeoutEventEmitted = false;
     auto timer = Timer{};
     timer.setInterval(std::chrono::milliseconds(3));
+    auto timeoutTime = long{0};
+    auto timerStartMs = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
     timer.timeout.connect([&]() {
         timeoutEventEmitted = true;
+        auto timeoutMs = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+        timeoutTime = timeoutMs - timerStartMs;
+        timer.stop();
     });
-
     timer.start();
-    std::this_thread::sleep_for(std::chrono::milliseconds(3));
-    dispatcher.exec();
+    REQUIRE(timer.isRunning());
+    REQUIRE_COMPARE_WITH_TIMEOUT(timeoutEventEmitted, true, std::chrono::milliseconds{1000});
+    CHECK_THAT(timeoutTime, IsBetweenMatcher(2, 4));
 
-    REQUIRE(timeoutEventEmitted == true);
+    auto timeout1EventEmitted = true;
+    auto timer1 = Timer{};
+    auto timeout1Time = long{0};
+    timer1.setInterval(std::chrono::milliseconds(5));
+    timerStartMs = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    timer1.timeout.connect([&]() {
+        timeout1EventEmitted = true;
+        auto timeoutMs = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+        timeout1Time = timeoutMs - timerStartMs;
+        timer1.stop();
+    });
+    timer1.start();
+    REQUIRE_COMPARE_WITH_TIMEOUT(timeout1EventEmitted, true, std::chrono::milliseconds{1000});
+    CHECK_THAT(timeout1Time, IsBetweenMatcher(4, 6));
 }
