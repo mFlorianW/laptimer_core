d6b4681d4ab34137b2fddf3026c7a6eefc108eee
Florian Weßel <florianwessel@gmx.net>
2024-05-23 20:29:57 +0200

    lappy_headless: start eventloop and handle linux sigint sigterm

--- programs/lappy_headless/lappy_headless.cpp
+++ programs/lappy_headless/lappy_headless.cpp
@@ -2,7 +2,32 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <EventLoop.hpp>
+#include <csignal>
+
+using namespace LaptimerCore::System;
+
+namespace
+{
+void signalHandler(int)
+{
+    auto el = EventLoop{};
+    el.postEvent(&el, std::make_unique<Event>(Event::Type::QuitEvent));
+}
+
+} // namespace
+
 int main(int argc, char** argv)
 {
+    EventLoop eventLoop;
+
+    // clang-format off
+    struct sigaction action{};
+    // clant-format on
+    action.sa_handler = signalHandler;
+    sigaction(SIGINT, &action, nullptr);
+    sigaction(SIGTERM, &action, nullptr);
+
+    eventLoop.exec();
     return 0;
 }
