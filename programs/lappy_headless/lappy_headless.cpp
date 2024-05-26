de6325b204cabf59f7c90269d145825971c19586
Florian Weßel <florianwessel@gmx.net>
2024-05-26 18:08:35 +0200

    lappy_headless: Add constant velocity date time provider

--- programs/lappy_headless/lappy_headless.cpp
+++ programs/lappy_headless/lappy_headless.cpp
@@ -2,10 +2,18 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <ConstantVelocityPositionDateTimeProvider.hpp>
 #include <EventLoop.hpp>
+#include <PositionData.hpp>
+#include <array>
 #include <csignal>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace LaptimerCore::System;
+using namespace LaptimerCore::Positioning;
 
 namespace
 {
@@ -13,6 +21,28 @@ void signalHandler(int)
 {
     auto el = EventLoop{};
     el.postEvent(&el, std::make_unique<Event>(Event::Type::QuitEvent));
+}
+
+std::vector<LaptimerCore::Common::PositionData> loadCsvPositions(std::string const filePath)
+{
+    auto positions = std::vector<LaptimerCore::Common::PositionData>{};
+    auto file = std::fstream(filePath);
+    auto line = std::string{};
+
+    while (std::getline(file, line)) {
+        std::istringstream input(line);
+        std::array<std::string, 2> splittedLine;
+        for (auto& i : splittedLine) {
+            getline(input, i, ',');
+        }
+
+        auto longitude = std::stof(splittedLine[0]);
+        auto latitude = std::stof(splittedLine[1]);
+
+        positions.emplace_back(latitude, longitude);
+    }
+
+    return positions;
 }
 
 } // namespace
@@ -27,6 +57,12 @@ int main(int argc, char** argv)
     action.sa_handler = signalHandler;
     sigaction(SIGINT, &action, nullptr);
     sigaction(SIGTERM, &action, nullptr);
+
+    // Load GPS position file
+    auto positions = loadCsvPositions("/home/florian/Coding/laptimer_core/laps/Oschersleben.csv");
+
+    auto positionProvider = ConstantVelocityPositionDateTimeProvider{positions};
+
 
     eventLoop.exec();
     return 0;
