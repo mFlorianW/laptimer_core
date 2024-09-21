696b11abddad0efebda543dda1c7b35b6a3f8c80
Florian Weßel <florianwessel@gmx.net>
2024-09-21 22:58:39 +0200

    lappy_sessiondl: Use database file path from global settings

--- programs/lappy_sessiondl/SessionDl/SessionDownloader.cpp
+++ programs/lappy_sessiondl/SessionDl/SessionDownloader.cpp
@@ -3,6 +3,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "SessionDownloader.hpp"
+#include <GlobalSettingsReader.hpp>
+#include <LoggingCategories.hpp>
+#include <QDebug>
 #include <QQmlContext>
 #include <QQuickWindow>
 
@@ -11,15 +14,27 @@ namespace LaptimerCore::SessionDl
 
 SessionDownloader::SessionDownloader(QHostAddress const& address, quint16 port) noexcept
 {
+    auto settingsBackend = Common::QSettingsBackend{};
+    auto const settings = Common::GlobalSettingsReader{&settingsBackend};
+    auto const dbFilePath = settings.getDbFilePath();
+    if (dbFilePath.isEmpty()) {
+        qCCritical(sessiondl) << "No database found please! Start session download from LappyShell.";
+    } else {
+        qCInfo(sessiondl) << "Sessions will be stored in database:" << dbFilePath;
+    }
+
+    mDatabase = std::make_unique<Storage::SqliteSessionDatabase>(dbFilePath.toStdString());
+    mMainWindowModel = std::make_unique<MainWindowModel>(mSessionDownloader, *mDatabase);
+
     auto qmlContext = mEngine.rootContext();
-    qmlContext->setContextProperty("g_MainWindowModel", &mMainWindowModel);
-    mEngine.load(QUrl{"qrc:/qml/MainWindow.qml"});
+    qmlContext->setContextProperty("g_MainWindowModel", mMainWindowModel.get());
+    mEngine.load(QUrl{"qrc:/qt/qml/Lappy/SessionDownloader/qml/MainWindow.qml"});
 
     mClient.setServerAddress(address.toString().toStdString());
     mClient.setServerPort(port);
 
-    mMainWindowModel.setHostAddress(address);
-    mMainWindowModel.setHostPort(port);
+    mMainWindowModel->setHostAddress(address);
+    mMainWindowModel->setHostPort(port);
 }
 
 void SessionDownloader::show() const noexcept
