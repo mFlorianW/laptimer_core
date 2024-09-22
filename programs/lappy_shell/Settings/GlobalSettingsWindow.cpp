3e8ae68473d8462f8ccef720a433798353fc28df
Florian Weßel <florianwessel@gmx.net>
2024-09-22 19:27:01 +0200

    lappy_shell: Setup GlobalSettingsWindow layout

--- programs/lappy_shell/Settings/GlobalSettingsWindow.cpp
+++ programs/lappy_shell/Settings/GlobalSettingsWindow.cpp
@@ -4,6 +4,7 @@
 
 #include "GlobalSettingsWindow.hpp"
 #include "ui_GlobalSettingsWindow.h"
+#include <QIcon>
 
 namespace LaptimerCore::LappyShell::Settings
 {
@@ -12,6 +13,19 @@ GlobalSettingsWindow::GlobalSettingsWindow()
     : mGlobalSettingsWindow{std::make_unique<Ui::GlobalSettingsWindow>()}
 {
     mGlobalSettingsWindow->setupUi(this);
+    mGlobalSettingsWindow->listWidget->setFixedWidth(150);
+
+    auto* settingsGeneral = mGlobalSettingsWindow->listWidget->item(0);
+    settingsGeneral->setIcon(QIcon{QStringLiteral(":/qt/qml/Lappy/Settings/icons/settings.svg")});
+    auto* deviceSettings = mGlobalSettingsWindow->listWidget->item(1);
+    deviceSettings->setIcon(QIcon{QStringLiteral(":/qt/qml/Lappy/Settings/icons/device.svg")});
+
+    auto* settingsListWidget = mGlobalSettingsWindow->listWidget;
+    connect(settingsListWidget, &QListWidget::currentRowChanged, this, [this](int currentRow) {
+        auto* item = mGlobalSettingsWindow->listWidget->item(currentRow);
+        mGlobalSettingsWindow->ContentGroupBox->setTitle(item->text());
+    });
+    settingsListWidget->setCurrentRow(0);
 }
 
 GlobalSettingsWindow::~GlobalSettingsWindow() = default;
